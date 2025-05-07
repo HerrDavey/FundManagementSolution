ea749bba27e7902b0f9e55aa903f35471f7f99dd
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2025-05-07 21:11:59 +0200

    Data migration update - CreatedDate as new param

--- Fundusze.Infrastucture/Migrations/20250507190815_AddCreatedDateToFund.Designer.cs
+++ Fundusze.Infrastucture/Migrations/20250507190815_AddCreatedDateToFund.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fundusze.Infrastucture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fundusze.Infrastucture.Migrations
 {
     [DbContext(typeof(FundsDbContext))]
-    partial class FundsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250507190815_AddCreatedDateToFund")]
+    partial class AddCreatedDateToFund
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
---
+++ Fundusze.Infrastucture/Migrations/20250507190815_AddCreatedDateToFund.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fundusze.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedDateToFund : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "DataUtworzenia",
+                table: "Funds",
+                newName: "CreatedDate");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PortfolioId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions",
+                column: "PortfolioId",
+                principalTable: "Portfolios",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Funds",
+                newName: "DataUtworzenia");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PortfolioId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Portfolios_PortfolioId",
+                table: "Transactions",
+                column: "PortfolioId",
+                principalTable: "Portfolios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
