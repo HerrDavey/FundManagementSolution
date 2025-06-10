0a897ca0fcadb887729e156992039361e8bbc5a1
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2025-06-10 18:21:15 +0200

    Frontend App Reconstruction due to unexpected issue

---
+++ Fundusze.BlazorAppServer/Program.cs
@@ -0,0 +1,40 @@
+using Fundusze.BlazorAppServer.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    // POPRAWKA TUTAJ: Ponownie w³¹czamy szczegó³owe b³êdy
+    .AddInteractiveServerComponents(options => options.DetailedErrors = true);
+
+// Rejestracja MudBlazor
+builder.Services.AddMudServices();
+
+// Rejestracja HttpClient
+builder.Services.AddScoped(sp =>
+{
+    var apiUrl = builder.Configuration["ApiBaseUrl"]
+                 ?? throw new InvalidOperationException("ApiBaseUrl is not configured.");
+    return new HttpClient { BaseAddress = new Uri(apiUrl) };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
