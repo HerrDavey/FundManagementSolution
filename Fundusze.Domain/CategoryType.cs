c401863924b36a3f90dd89c4ff54705f96217f06
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2025-06-14 22:07:23 +0200

    Updated Transactions

---
+++ Fundusze.Domain/CategoryType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundusze.Domain
+{
+    public enum CategoryType
+    {
+        Akcja,
+        Obligacja,
+        Mieszany,
+        ETF
+    }
+}
